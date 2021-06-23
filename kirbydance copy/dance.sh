f4564868f5daa93170e7bfe47bc8bffede1a58cf
Raul Alvarado <raul.alvarado@appliedis.com>
2021-06-23 08:56:58 -0400

    minor updates

--- kirbydance copy/dance.sh
+++ kirbydance copy/dance.sh
@@ -1,6 +1,14 @@
 echo "<(^.^<) <(^.^)> (>^.^)>"
-
-df -h
-cd /mnt/input-data
-touch test.txt
+cd /mnt/input_data/input_data
+pwd
 ls -la
+echo "input_file is ${INPUT_FILE}"
+ls -la ${INPUT_FILE}
+echo "output dir is : ${OUTPUT_DIR}"
+mkdir ${OUTPUT_DIR}
+RANDOM_NUMBER=$RANDOM
+FILENAME="${OUTPUT_DIR}/$RANDOM_NUMBER.tmp"
+echo "why would you even look in here??!!!!" > $FILENAME
+cd ${OUTPUT_DIR}
+ls -la
+pwd
